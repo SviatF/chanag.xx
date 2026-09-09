2ed8d70dd6af1ef7b0b7948aa5faad169c660150
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-09 15:55:30 -0700

    Update robots sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,2 @@
 import type {MetadataRoute} from "next";
-export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://panchang.in/sitemap.xml"}}
+export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://panchvani.com/sitemap.xml"}}
